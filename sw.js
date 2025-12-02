d2041ac20f39909790c3fff43e696da38df1d12e
mirnasir007 <mir703876@gmail.com>
2025-12-02 13:03:56 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+{
+  "name": "PFF Attendance Premium",
+  "short_name": "PFF Attendance",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#f4f6f9",
+  "theme_color": "#0d6efd",
+  "orientation": "portrait-primary",
+  "icons": [
+    {
+      "src": "logo.png",
+      "type": "image/png",
+      "sizes": "192x192"
+    },
+    {
+      "src": "logo.png",
+      "type": "image/png",
+      "sizes": "512x512"
+    }
+  ]
+}
